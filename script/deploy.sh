a3e6f80ee1b42b7508aef4ec931f199743242c30
Andrea Ciantelli <andrea.ciantelli@sportwig.com>
2023-09-18 19:46:25 +0200

    updated app deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,30 @@
+# arguments
+
+pararser() {
+    # Define default values
+    appname=${appname:-"defaultApp"}
+    region=${region:-"eu-west-1"}
+    cfgtemplate=${cfgtemplate:-"elasticbeanstalk-whitelabel"}
+    environment=${environment:-"master"}
+
+    # Assign the values given by the user
+    while [ $# -gt 0 ]; do
+        if [[ $1 == *"--"* ]]; then
+            param="${1/--/}"
+            declare -g $param="$2"
+        fi
+        shift
+    done
+
+}
+pararser $@
+
+eb init --region eu-west-1 --platform "Docker running on 64bit Amazon Linux 2023" $appname --region $region
+eb use $appname-$environment
+
+# get s3 bucket for region
+json_output=$(aws elasticbeanstalk describe-environment-resources --environment-name $appname-$environment --region $region)
+instance=$(echo "$json_output" | jq -r '.EnvironmentResources.Instances[0].Id')
+
+echo "instance " $instance
+chmod 0400 ~/.ssh/$appname-$environment.pem
